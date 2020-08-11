9cb4d53ab52dd09cc46c6930470a7d97b0c336ed
Nelson Dane <danelego15@gmail.com>
2020-08-11 15:49:31 -0400

    Starting tweak!

---
+++ tweakschedulerpreferences/TSPRootListController.m
@@ -0,0 +1,13 @@
+#include "TSPRootListController.h"
+
+@implementation TSPRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
